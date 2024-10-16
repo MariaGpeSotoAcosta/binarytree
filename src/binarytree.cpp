83d20476634e60f62ada6feb8868d7b75c8c4622
MariaGpeSotoAcosta <A00228158@tec.mx>
2024-10-15 21:02:25 -0600

    Update binarytree.cpp

--- src/binarytree.cpp
+++ src/binarytree.cpp
@@ -5,24 +5,96 @@ data=value;
 left=nullptr;
 right=nullptr;
 }
-binarytree::binarytree()
+BinaryTree::BinaryTree()
 {
     root = nullptr;
 }
 
-Node* BinaryTree::insert(Node *node, int value){
-    if(node==nullptr){
+Node* BinaryTree::Insert(Node* node, int value) {
+    if (node == nullptr) {
         return new Node(value);
     }
-    if(value < node->data){
-        node->left = insert(node->left, value);
-    }else{
-    node->right = insert(node->right, value);
+
+    if (value < node->Data) {
+        node->Left = Insert(node->Left, value);
+    } else if (value > node->Data) {
+        node->Right = Insert(node->Right, value);
+    }
+
+    return node;
+}
+
+Node* BinaryTree::Search(Node* node, int value) {
+    if (node == nullptr || node->Data == value) {
+        return node;
+    }
+
+    if (value < node->Data) {
+        return Search(node->Left, value);
+    }
+
+    return Search(node->Right, value);
+}
+
+Node* BinaryTree::Remove(Node* node, int value) {
+    if (node == nullptr) {
+        return node;
+    }
+
+    if (value < node->Data) {
+        node->Left = Remove(node->Left, value);
+    } else if (value > node->Data) {
+        node->Right = Remove(node->Right, value);
+    } else {
+        if (node->Left == nullptr) {
+            Node* temp = node->Right;
+            delete node;
+            return temp;
+        } else if (node->Right == nullptr) {
+            Node* temp = node->Left;
+            delete node;
+            return temp;
+        }
+
+        Node* temp = FindMin(node->Right);
+        node->Data = temp->Data;
+        node->Right = Remove(node->Right, temp->Data);
     }
     return node;
 }
 
-binarytree::~binarytree()
+Node* BinaryTree::FindMin(Node* node) {
+    while (node->Left != nullptr) {
+        node = node->Left;
+    }
+    return node;
+}
+
+void BinaryTree::InOrderTraversal(Node* node) {
+    if (node != nullptr) {
+        InOrderTraversal(node->Left);
+        cout << node->Data << " ";
+        InOrderTraversal(node->Right);
+    }
+}
+
+void BinaryTree::PreOrderTraversal(Node* node) {
+    if (node != nullptr) {
+        cout << node->Data << " ";
+        PreOrderTraversal(node->Left);
+        PreOrderTraversal(node->Right);
+    }
+}
+
+void BinaryTree::PostOrderTraversal(Node* node) {
+    if (node != nullptr) {
+        PostOrderTraversal(node->Left);
+        PostOrderTraversal(node->Right);
+        cout << node->Data << " ";
+    }
+}
+
+BinaryTree::~BinaryTree()
 {
     //dtor
 }
