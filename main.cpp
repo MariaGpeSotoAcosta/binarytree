7d11efa9f9fa5c196029781ca5bceef1934f8081
MariaGpeSotoAcosta <A00228158@tec.mx>
2024-10-15 22:30:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,31 +14,7 @@ int main()
     tree.root = tree.insert(tree.root, 60);
     tree.root = tree.insert(tree.root, 80);
 
-    cout << "Recorrido in-orden: ";
-    tree.inOrderTraversal(tree.root);
-    cout << endl;
-
-    cout << "Recorrido pre-orden: ";
-    tree.preOrderTraversal(tree.root);
-    cout << endl;
-
-    cout << "Recorrido post-orden: ";
-    tree.postOrderTraversal(tree.root);
-    cout << endl;
-
-    int valueToSearch = 30;
-    Node* result = tree.search(tree.root, valueToSearch);
-    if (result != nullptr) {
-        cout << "Valor " << valueToSearch << " encontrado en el árbol." << endl;
-    } else {
-        cout << "Valor " << valueToSearch << " no encontrado en el árbol." << endl;
-    }
-
-    int valueToRemove = 30;
-    tree.root = tree.remove(tree.root, valueToRemove);
-    cout << "Después de eliminar " << valueToRemove << ", recorrido in-orden: ";
-    tree.inOrderTraversal(tree.root);
-    cout << endl;
+ 
 
     return 0;
 }
