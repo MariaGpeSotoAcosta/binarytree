70bb479b3f2d74173da106f371b30f8c1c9cf179
MariaGpeSotoAcosta <A00228158@tec.mx>
2024-10-15 21:00:04 -0600

    Update binarytree.h

--- include/binarytree.h
+++ include/binarytree.h
@@ -7,14 +7,21 @@ Node* right;
 Node* left;
 Node(int value);
 };
-class binarytree
-{
-    public:
-        Node* root;
-        binarytree();
-        virtual ~binarytree();
+class BinaryTree {
+public:
+    Node* Root;
+
     BinaryTree();
-    Node* insert(Node* node, int value);
-    //void preorder(Node* node);
+    Node* Insert(Node* node, int value);
+    Node* Search(Node* node, int value);
+    Node* Remove(Node* node, int value);
+    void InOrderTraversal(Node* node);
+    void PreOrderTraversal(Node* node);
+    void PostOrderTraversal(Node* node);
+
+private:
+    Node* FindMin(Node* node);
+};
 
 #endif // BINARYTREE_H
+
